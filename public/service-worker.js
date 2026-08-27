88020e244efdeb9a7397dafa8a0a8361a0d161ac
Guilherme Trevisan <guilherme.ztrevisan@gmail.com>
2026-08-27 11:37:51 -0300

    Ajusta identidade na impressão de envelopes

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 const CACHE_NAME =
-  'hiperion-protocolos-v55-label-due-date';
+  'hiperion-protocolos-v56-envelope-brand';
 
 
 const APP_SHELL = [
